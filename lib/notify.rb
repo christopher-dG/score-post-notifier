b2f7b9da3a3b16695ac092bf3c7e4f4205eb2e16
Chris de Graaf <chrisadegraaf@gmail.com>
2017-08-11 04:24:39 +0000

    Add reactions

--- lib/notify.rb
+++ lib/notify.rb
@@ -3,6 +3,8 @@ require 'discordrb'
 require 'yaml'
 
 SP_REGEX = /.+\|.+-.+\[.+\]/  # This is what a score post looks like.
+UP = "\u{1F44D}"
+DOWN = "\u{1F44E}"
 
 post_ids = File.join(Dir.home, 'score-post-notifier', 'posts')
 config = YAML.load_file(File.join(Dir.home, 'score-post-notifier', 'config.yml'))
@@ -29,9 +31,11 @@ Redd.it(
   if post.title.strip =~ SP_REGEX && !post.is_self && !ids.include?(post.id)
     ids.push(post.id)
     config['discord_channel_ids_to_roles'].each do |channel, role|
-      discord.send_message(
+      msg = discord.send_message(
         channel, "#{role}: #{post.title}\nhttps://redd.it/#{post.id}"
       )
+      msg.react(UP)
+      msg.react(DOWN)
     end
     puts("Sent a message for post: redd.it/#{post.id}")
   end
